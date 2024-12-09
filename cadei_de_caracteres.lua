3f77466dd811e152816599c2979183c3218ff160
douglas-sola <v671408@gmail.com>
2024-12-09 18:18:26 -0300

    sal

--- cadei_de_caracteres.lua
+++ cadei_de_caracteres.lua
@@ -5,6 +5,7 @@ b = "pomba"
 can = "'sal'"
 sal = '"saa"'
 exe = "goste('gostei')"
+ss = 123
 print(can)
 print(sal)
 print(exe)
@@ -15,9 +16,28 @@ print(as)
 aux = string.gsub(as,"broxa",outro)
 print (aux)
 print ("tamanho da variavel:> " .. #outro)--para length no lua usa #
+
+print (outro .."\n" .. "\a")
+print("15" + "5")
+
+print (tonumber(123) * 2)
+print(tostring(1223) == "1223")
+
 --comentari de varios blocos
---[[
+--
+----[[
 --feito: 09/12/2024 as 16:15
 --a@vini
+--
+-- \a Campainha
+-- \b backspace
+-- \f Alinhamento de formulario
+-- \n quebra linha
+-- \r Retorno de carro
+-- \t Tabulação horizontal
+-- \v tabulação vertical
+-- \\ barra invertida
+-- \' aspotrofo
+-- \" aspas
 --]]
 
